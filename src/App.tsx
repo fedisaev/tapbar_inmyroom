958c94a7a9ded496350d980b1a8f892996ad10fa
fedisaev <127963492+Fed0102@users.noreply.github.com>
2024-03-27 12:29:47 +0300

    add app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,63 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import shareImage from './assets/share-icon.png';
+import './App.scss';
+import {useState} from "react";
+import shareIcon from './assets/share-icon.svg';
+import scrollUpIcon from './assets/scroll-up-icon.svg';
+import commentsIcon from './assets/comments-icon.svg';
+import heartIcon from './assets/heart-icon.svg';
 
 function App() {
 
+    const [tapBar, setTapBar] = useState(true);
+    const [comments, setComments] = useState(0);
+    const [liked, setLiked] = useState(0);
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY >= 200) {
+            setTapBar(false);
+        } else {
+            setTapBar(true);
+        }
+    });
+
+    const sharePage = () => {
+        if (navigator.share) {
+            navigator.share({url: window.location.href});
+        } else {
+            navigator.clipboard.writeText(window.location.href)
+                .then(() => alert('Ссылка скопирована в буфер обмена'))
+                .catch((error) => console.error('Ошибка при копировании в буфер обмена:', error));
+        }
+    };
+
+    function addComments() {
+        setComments(prevState => prevState + 1);
+    }
+
+    function addLiked() {
+        setLiked(prevState => prevState + 1);
+    }
+
+    function scrollToTop() {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
+    }
+
     return (
-        <>
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo"/>
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo"/>
-                </a>
+        <main>
+            <div className='app'>
+                <p className='text'>Одноэтажный дом с двумя спальнями в стиле эклектика</p>
             </div>
-            <img src={shareImage} alt="share-icon"/>
-            <h1>TAPBAR</h1>
-        </>
+            <div className='scrollDiv'></div>
+            <ul className={tapBar ? 'list' : 'list hidden'}>
+                <li onClick={sharePage}><img src={shareIcon} alt="share-icon"/></li>
+                <li onClick={scrollToTop}><img src={scrollUpIcon} alt="scroll-up-icon"/></li>
+                <li onClick={addComments}><img src={commentsIcon} alt="comments-icon"/><span>{comments}</span></li>
+                <li onClick={addLiked}><img src={heartIcon} alt="heart-icon"/><span>{liked}</span></li>
+            </ul>
+        </main>
     )
 }
 
-export default App
+export default App;
